3c0953c52a1abff5b999a73a66a1a6d11e27724b
pwojcieszak <piotrekw1002@gmail.com>
2024-10-27 13:20:50 +0100

    Reversed string building in LLVM

--- src/GeneratorLLVM copy.hs
+++ src/GeneratorLLVM copy.hs
@@ -10,107 +10,116 @@ isAllocated ident varMap = ident `elem` varMap
 
 generateLLVM :: AbsInstant.Program -> String
 generateLLVM (Prog stmts) = 
-  let (code, _, _) = foldl generateStmt ("", 0, []) stmts
+  let (code, _, _) = foldl generateStmt ([], 0, []) stmts
   in "@.str = private unnamed_addr constant [4 x i8] c\"%d\\0A\\00\", align 1\n\n" ++
      "declare i32 @printf(i8*, ...) #1\n\n" ++
      "define i32 @main() {\n" ++
-     code ++
+     concat (reverse code) ++
      "  ret i32 0\n" ++
      "}\n"
 
-generateStmt :: (String, Int, VarMap) -> AbsInstant.Stmt -> (String, Int, VarMap)
-generateStmt (code, reg, varMap) (SAss (Ident ident) exp) =
-  let (expCode, regAfterExp) = generateExp reg exp
-      allocCode = if isAllocated ident varMap
+generateStmt :: ([String], Int, VarMap) -> AbsInstant.Stmt -> ([String], Int, VarMap)
+generateStmt (code, reg, varMap) (SAss (Ident ident) (ExpLit l)) =
+  let allocCode = if isAllocated ident varMap
                   then ""
                   else "  %" ++ ident ++ " = alloca i32\n"
+      storeCode = "  store i32 " ++ show (l) ++ ", i32* %" ++ ident ++ "\n"
+      newVarMap = if isAllocated ident varMap then varMap else ident : varMap
+  in (storeCode : (allocCode : code), reg, newVarMap)
+
+generateStmt (code, reg, varMap) (SAss (Ident ident) exp) =
+  let allocCode = if isAllocated ident varMap
+                  then ""
+                  else "  %" ++ ident ++ " = alloca i32\n"
+      (expCode, regAfterExp) = generateExp (allocCode : code) reg exp
       storeCode = "  store i32 %r" ++ show (regAfterExp - 1) ++ ", i32* %" ++ ident ++ "\n"
       newVarMap = if isAllocated ident varMap then varMap else ident : varMap
-  in (code ++ allocCode ++ expCode ++ storeCode, regAfterExp, newVarMap)
+  in (storeCode : expCode, regAfterExp, newVarMap)
+
+generateStmt (code, reg, varMap) (SExp (ExpLit l)) =
+  let printCall = "  call i32 (i8*, ...) @printf(i8* getelementptr ([4 x i8], [4 x i8]* @.str, i32 0, i32 0), i32 " ++ show l ++ ")\n"
+  in (printCall : code, reg, varMap)
+
 generateStmt (code, reg, varMap) (SExp exp) =
-  let (expCode, newReg) = generateExp reg exp
-  in (code ++ expCode ++ "  call i32 (i8*, ...) @printf(i8* getelementptr ([4 x i8], [4 x i8]* @.str, i32 0, i32 0), i32 %r" ++ show (newReg - 1) ++ ")\n", newReg, varMap)
+  let (expCode, newReg) = generateExp code reg exp
+      printCall = "  call i32 (i8*, ...) @printf(i8* getelementptr ([4 x i8], [4 x i8]* @.str, i32 0, i32 0), i32 %r" ++ show (newReg - 1) ++ ")\n"
+  in (printCall : expCode, newReg, varMap)
 
-generateExp :: Int -> AbsInstant.Exp -> (String, Int)
-generateExp reg (ExpAdd e1 e2) =
-  case (e1, e2) of
-    (ExpLit n1, ExpLit n2) -> 
-      let addCode = "  %r" ++ show reg ++ " = add i32 " ++ show n1 ++ ", " ++ show n2 ++ "\n"
-      in (addCode, reg + 1)
-    (ExpLit n, _) -> 
-      let (code, reg1) = generateExp reg e2
-          addCode = "  %r" ++ show reg1 ++ " = add i32 " ++ show n ++ ", %r" ++ show (reg - 1) ++ "\n"
-      in (code ++ addCode, reg1 + 1)
-    (_, ExpLit n) -> 
-      let (code, reg1) = generateExp reg e1
-          addCode = "  %r" ++ show reg1 ++ " = add i32 %r" ++ show (reg - 1) ++ ", " ++ show n ++ "\n"
-      in (code ++ addCode, reg1 + 1)
-    _ -> 
-      let (code1, reg1) = generateExp reg e1
-          (code2, reg2) = generateExp reg1 e2
-          additionCode = "  %r" ++ show reg2 ++ " = add i32 %r" ++ show (reg1 - 1) ++ ", %r" ++ show (reg2 - 1) ++ "\n"
-      in (code1 ++ code2 ++ additionCode, reg2 + 1)
 
-generateExp reg (ExpSub e1 e2) =
-  case (e1, e2) of
-    (ExpLit n1, ExpLit n2) -> 
-      let subCode = "  %r" ++ show reg ++ " = sub i32 " ++ show n1 ++ ", " ++ show n2 ++ "\n"
-      in (subCode, reg + 1)
-    (ExpLit n, _) -> 
-      let (code, reg1) = generateExp reg e2
-          subCode = "  %r" ++ show reg1 ++ " = sub i32 " ++ show n ++ ", %r" ++ show (reg - 1) ++ "\n"
-      in (code ++ subCode, reg1 + 1)
-    (_, ExpLit n) -> 
-      let (code, reg1) = generateExp reg e1
-          subCode = "  %r" ++ show reg1 ++ " = sub i32 %r" ++ show (reg - 1) ++ ", " ++ show n ++ "\n"
-      in (code ++ subCode, reg1 + 1)
-    _ -> 
-      let (code1, reg1) = generateExp reg e1
-          (code2, reg2) = generateExp reg1 e2
-          subtractionCode = "  %r" ++ show reg2 ++ " = sub i32 %r" ++ show (reg1 - 1) ++ ", %r" ++ show (reg2 - 1) ++ "\n"
-      in (code1 ++ code2 ++ subtractionCode, reg2 + 1)
+generateExp :: [String] -> Int -> AbsInstant.Exp -> ([String], Int)
+generateExp code reg (ExpAdd (ExpLit l1) (ExpLit l2)) =
+  let addCode = "  %r" ++ show reg ++ " = add i32 " ++ show l1 ++ ", " ++ show l2 ++ "\n"
+  in (addCode : code, reg + 1)
+generateExp code reg (ExpAdd (ExpLit l) e) =
+  let (expCode, reg1) = generateExp code reg e
+      addCode = "  %r" ++ show reg1 ++ " = add i32 " ++ show l ++ ", %r" ++ show (reg1 - 1) ++ "\n"
+  in (addCode : expCode, reg1 + 1)
+generateExp code reg (ExpAdd e (ExpLit l)) =
+  let (expCode, reg1) = generateExp code reg e
+      addCode = "  %r" ++ show reg1 ++ " = add i32 %r" ++ show (reg1 - 1) ++ ", " ++ show l ++ "\n"
+  in (addCode : code, reg1 + 1)
+generateExp code reg (ExpAdd e1 e2) =
+  let (code2, reg2) = generateExp code reg e2
+      (code1, reg1) = generateExp code2 reg2 e1
+      additionCode = "  %r" ++ show reg2 ++ " = add i32 %r" ++ show (reg1 - 1) ++ ", %r" ++ show (reg2 - 1) ++ "\n"
+  in (additionCode : code1, reg2 + 1)
 
-generateExp reg (ExpMul e1 e2) =
-  case (e1, e2) of
-    (ExpLit n1, ExpLit n2) -> 
-      let mulCode = "  %r" ++ show reg ++ " = mul i32 " ++ show n1 ++ ", " ++ show n2 ++ "\n"
-      in (mulCode, reg + 1)
-    (ExpLit n, _) -> 
-      let (code, reg1) = generateExp reg e2
-          mulCode = "  %r" ++ show reg1 ++ " = mul i32 " ++ show n ++ ", %r" ++ show (reg - 1) ++ "\n"
-      in (code ++ mulCode, reg1 + 1)
-    (_, ExpLit n) -> 
-      let (code, reg1) = generateExp reg e1
-          mulCode = "  %r" ++ show reg1 ++ " = mul i32 %r" ++ show (reg - 1) ++ ", " ++ show n ++ "\n"
-      in (code ++ mulCode, reg1 + 1)
-    _ -> 
-      let (code1, reg1) = generateExp reg e1
-          (code2, reg2) = generateExp reg1 e2
-          multiplicationCode = "  %r" ++ show reg2 ++ " = mul i32 %r" ++ show (reg1 - 1) ++ ", %r" ++ show (reg2 - 1) ++ "\n"
-      in (code1 ++ code2 ++ multiplicationCode, reg2 + 1)
+generateExp code reg (ExpSub (ExpLit l1) (ExpLit l2)) =
+  let subCode = "  %r" ++ show reg ++ " = sub i32 " ++ show l1 ++ ", " ++ show l2 ++ "\n"
+  in (subCode : code, reg + 1)
+generateExp code reg (ExpSub (ExpLit l) e) =
+  let (code1, reg1) = generateExp code reg e
+      subCode = "  %r" ++ show reg1 ++ " = sub i32 " ++ show l ++ ", %r" ++ show (reg1 - 1) ++ "\n"
+  in (subCode : code1, reg1 + 1)
+generateExp code reg (ExpSub e (ExpLit l)) =
+  let (code1, reg1) = generateExp code reg e
+      subCode = "  %r" ++ show reg1 ++ " = sub i32 %r" ++ show (reg1 - 1) ++ ", " ++ show l ++ "\n"
+  in (subCode : code1, reg1 + 1)
+generateExp code reg (ExpSub e1 e2) =
+  let (code2, reg2) = generateExp code reg e2
+      (code1, reg1) = generateExp code2 reg2 e1
+      subCode = "  %r" ++ show reg2 ++ " = sub i32 %r" ++ show (reg1 - 1) ++ ", %r" ++ show (reg2 - 1) ++ "\n"
+  in (subCode : code1, reg2 + 1)
 
-generateExp reg (ExpDiv e1 e2) =
-  case (e1, e2) of
-    (ExpLit n1, ExpLit n2) -> 
-      let divCode = "  %r" ++ show reg ++ " = sdiv i32 " ++ show n1 ++ ", " ++ show n2 ++ "\n"
-      in (divCode, reg + 1)
-    (ExpLit n, _) -> 
-      let (code, reg1) = generateExp reg e2
-          divCode = "  %r" ++ show reg1 ++ " = sdiv i32 " ++ show n ++ ", %r" ++ show (reg - 1) ++ "\n"
-      in (code ++ divCode, reg1 + 1)
-    (_, ExpLit n) -> 
-      let (code, reg1) = generateExp reg e1
-          divCode = "  %r" ++ show reg1 ++ " = sdiv i32 %r" ++ show (reg - 1) ++ ", " ++ show n ++ "\n"
-      in (code ++ divCode, reg1 + 1)
-    _ -> 
-      let (code1, reg1) = generateExp reg e1
-          (code2, reg2) = generateExp reg1 e2
-          divisionCode = "  %r" ++ show reg2 ++ " = sdiv i32 %r" ++ show (reg1 - 1) ++ ", %r" ++ show (reg2 - 1) ++ "\n"
-      in (code1 ++ code2 ++ divisionCode, reg2 + 1)
+generateExp code reg (ExpMul (ExpLit l1) (ExpLit l2)) =
+  let mulCode = "  %r" ++ show reg ++ " = mul i32 " ++ show l1 ++ ", " ++ show l2 ++ "\n"
+  in (mulCode : code, reg + 1)
+generateExp code reg (ExpMul (ExpLit l) e) =
+  let (code1, reg1) = generateExp code reg e
+      mulCode = "  %r" ++ show reg1 ++ " = mul i32 " ++ show l ++ ", %r" ++ show (reg1 - 1) ++ "\n"
+  in (mulCode : code1, reg1 + 1)
+generateExp code reg (ExpMul e (ExpLit l)) =
+  let (code1, reg1) = generateExp code reg e
+      mulCode = "  %r" ++ show reg1 ++ " = mul i32 %r" ++ show (reg1 - 1) ++ ", " ++ show l ++ "\n"
+  in (mulCode : code1, reg1 + 1)
+generateExp code reg (ExpMul e1 e2) =
+  let (code2, reg2) = generateExp code reg e2
+      (code1, reg1) = generateExp code2 reg2 e1
+      mulCode = "  %r" ++ show reg2 ++ " = mul i32 %r" ++ show (reg1 - 1) ++ ", %r" ++ show (reg2 - 1) ++ "\n"
+  in (mulCode : code1, reg2 + 1)
 
-generateExp reg (ExpVar (Ident ident)) =
+generateExp code reg (ExpDiv (ExpLit l1) (ExpLit l2)) =
+  let divCode = "  %r" ++ show reg ++ " = sdiv i32 " ++ show l1 ++ ", " ++ show l2 ++ "\n"
+  in (divCode : code, reg + 1)
+generateExp code reg (ExpDiv (ExpLit l) e) =
+  let (code1, reg1) = generateExp code reg e
+      divCode = "  %r" ++ show reg1 ++ " = sdiv i32 " ++ show l ++ ", %r" ++ show (reg1 - 1) ++ "\n"
+  in (divCode : code1, reg1 + 1)
+generateExp code reg (ExpDiv e (ExpLit l)) =
+  let (code1, reg1) = generateExp code reg e
+      divCode = "  %r" ++ show reg1 ++ " = sdiv i32 %r" ++ show (reg1 - 1) ++ ", " ++ show l ++ "\n"
+  in (divCode : code1, reg1 + 1)
+generateExp code reg (ExpDiv e1 e2) =
+  let (code2, reg2) = generateExp code reg e2
+      (code1, reg1) = generateExp code2 reg2 e1
+      divCode = "  %r" ++ show reg2 ++ " = sdiv i32 %r" ++ show (reg1 - 1) ++ ", %r" ++ show (reg2 - 1) ++ "\n"
+  in (divCode : code1, reg2 + 1)
+
+
+generateExp code reg (ExpLit n) =
+  let litCode = "  " ++ show n ++ "\n"
+  in (litCode : code, reg)
+
+generateExp code reg (ExpVar (Ident ident)) =
   let loadCode = "  %r" ++ show reg ++ " = load i32, i32* %" ++ ident ++ "\n"
-  in (loadCode, reg + 1)
-
-
-
+  in (loadCode : code, reg + 1)
--- src/GeneratorLLVM.hs
+++ src/GeneratorLLVM.hs
@@ -10,99 +10,69 @@ isAllocated ident varMap = ident `elem` varMap
 
 generateLLVM :: AbsInstant.Program -> String
 generateLLVM (Prog stmts) = 
-  let (code, _, _) = foldl generateStmt ("", 0, []) stmts
+  let (code, _, _) = foldl generateStmt ([], 0, []) stmts
   in "@.str = private unnamed_addr constant [4 x i8] c\"%d\\0A\\00\", align 1\n\n" ++
      "declare i32 @printf(i8*, ...) #1\n\n" ++
      "define i32 @main() {\n" ++
-     code ++
+     concat (reverse code) ++
      "  ret i32 0\n" ++
      "}\n"
 
-generateStmt :: (String, Int, VarMap) -> AbsInstant.Stmt -> (String, Int, VarMap)
+generateStmt :: ([String], Int, VarMap) -> AbsInstant.Stmt -> ([String], Int, VarMap)
 generateStmt (code, reg, varMap) (SAss (Ident ident) (ExpLit l)) =
   let allocCode = if isAllocated ident varMap
                   then ""
                   else "  %" ++ ident ++ " = alloca i32\n"
-      storeCode = "  store i32 " ++ show (l) ++ ", i32* %" ++ ident ++ "\n"
+      storeCode = "  store i32 " ++ show l ++ ", i32* %" ++ ident ++ "\n"
       newVarMap = if isAllocated ident varMap then varMap else ident : varMap
-  in (code ++ allocCode ++ storeCode, reg, newVarMap)
+  in (storeCode : (allocCode : code), reg, newVarMap)
 
 generateStmt (code, reg, varMap) (SAss (Ident ident) exp) =
-  let (expCode, regAfterExp) = generateExp reg exp
-      allocCode = if isAllocated ident varMap
+  let allocCode = if isAllocated ident varMap
                   then ""
                   else "  %" ++ ident ++ " = alloca i32\n"
+      (expCode, regAfterExp) = generateExp (allocCode : code) reg exp
       storeCode = "  store i32 %r" ++ show (regAfterExp - 1) ++ ", i32* %" ++ ident ++ "\n"
       newVarMap = if isAllocated ident varMap then varMap else ident : varMap
-  in (code ++ allocCode ++ expCode ++ storeCode, regAfterExp, newVarMap)
+  in (storeCode : expCode, regAfterExp, newVarMap)
 
 generateStmt (code, reg, varMap) (SExp (ExpLit l)) =
-  (code ++ "  call i32 (i8*, ...) @printf(i8* getelementptr ([4 x i8], [4 x i8]* @.str, i32 0, i32 0), i32 " ++ show l ++ ")\n", reg, varMap)
+  let printCall = "  call i32 (i8*, ...) @printf(i8* getelementptr ([4 x i8], [4 x i8]* @.str, i32 0, i32 0), i32 " ++ show l ++ ")\n"
+  in (printCall : code, reg, varMap)
 
 generateStmt (code, reg, varMap) (SExp exp) =
-  let (expCode, newReg) = generateExp reg exp
-  in (code ++ expCode ++ "  call i32 (i8*, ...) @printf(i8* getelementptr ([4 x i8], [4 x i8]* @.str, i32 0, i32 0), i32 %r" ++ show (newReg - 1) ++ ")\n", newReg, varMap)
+  let (expCode, newReg) = generateExp code reg exp
+      printCall = "  call i32 (i8*, ...) @printf(i8* getelementptr ([4 x i8], [4 x i8]* @.str, i32 0, i32 0), i32 %r" ++ show (newReg - 1) ++ ")\n"
+  in (printCall : expCode, newReg, varMap)
 
-generateExp :: Int -> AbsInstant.Exp -> (String, Int)
-generateExp reg (ExpAdd (ExpLit l1) (ExpLit l2)) =
-  ("  %r" ++ show reg ++ " = add i32 " ++ show l1 ++ ", " ++ show l2 ++ "\n", reg + 1)
-generateExp reg (ExpAdd (ExpLit l) e) =
-  let (code, reg1) = generateExp reg e
-  in (code ++ "  %r" ++ show reg1 ++ " = add i32 " ++ show l ++ ", %r" ++ show (reg1 - 1) ++ "\n", reg1 + 1)
-generateExp reg (ExpAdd e (ExpLit l)) =
-  let (code, reg1) = generateExp reg e
-  in (code ++ "  %r" ++ show reg1 ++ " = add i32 %r" ++ show (reg1 - 1) ++ ", " ++ show l ++ "\n", reg1 + 1)
-generateExp reg (ExpAdd e1 e2) =
-  let (code1, reg1) = generateExp reg e1
-      (code2, reg2) = generateExp reg1 e2
-      additionCode = "  %r" ++ show reg2 ++ " = add i32 %r" ++ show (reg1 - 1) ++ ", %r" ++ show (reg2 - 1) ++ "\n"
-  in (code1 ++ code2 ++ additionCode, reg2 + 1)
+generateExp :: [String] -> Int -> AbsInstant.Exp -> ([String], Int)
+generateExp code reg (ExpAdd e1 e2) = generateBinaryOp code reg e1 e2 "add"
+generateExp code reg (ExpSub e1 e2) = generateBinaryOp code reg e1 e2 "sub"
+generateExp code reg (ExpMul e1 e2) = generateBinaryOp code reg e1 e2 "mul"
+generateExp code reg (ExpDiv e1 e2) = generateBinaryOp code reg e1 e2 "sdiv"
+generateExp code reg (ExpLit n) =
+  let litCode = "  " ++ show n ++ "\n"
+  in (litCode : code, reg + 1)
 
-generateExp reg (ExpSub (ExpLit l1) (ExpLit l2)) =
-  ("  %r" ++ show reg ++ " = sub i32 " ++ show l1 ++ ", " ++ show l2 ++ "\n", reg + 1)
-generateExp reg (ExpSub (ExpLit l) e) =
-  let (code, reg1) = generateExp reg e
-  in (code ++ "  %r" ++ show reg1 ++ " = sub i32 " ++ show l ++ ", %r" ++ show (reg1 - 1) ++ "\n", reg1 + 1)
-generateExp reg (ExpSub e (ExpLit l)) =
-  let (code, reg1) = generateExp reg e
-  in (code ++ "  %r" ++ show reg1 ++ " = sub i32 %r" ++ show (reg1 - 1) ++ ", " ++ show l ++ "\n", reg1 + 1)
-generateExp reg (ExpSub e1 e2) =
-  let (code1, reg1) = generateExp reg e1
-      (code2, reg2) = generateExp reg1 e2
-      subtractionCode = "  %r" ++ show reg2 ++ " = sub i32 %r" ++ show (reg1 - 1) ++ ", %r" ++ show (reg2 - 1) ++ "\n"
-  in (code1 ++ code2 ++ subtractionCode, reg2 + 1)
-
-generateExp reg (ExpMul (ExpLit l1) (ExpLit l2)) =
-  ("  %r" ++ show reg ++ " = mul i32 " ++ show l1 ++ ", " ++ show l2 ++ "\n", reg + 1)
-generateExp reg (ExpMul (ExpLit l) e) =
-  let (code, reg1) = generateExp reg e
-  in (code ++ "  %r" ++ show reg1 ++ " = mul i32 " ++ show l ++ ", %r" ++ show (reg1 - 1) ++ "\n", reg1 + 1)
-generateExp reg (ExpMul e (ExpLit l)) =
-  let (code, reg1) = generateExp reg e
-  in (code ++ "  %r" ++ show reg1 ++ " = mul i32 %r" ++ show (reg1 - 1) ++ ", " ++ show l ++ "\n", reg1 + 1)
-generateExp reg (ExpMul e1 e2) =
-  let (code1, reg1) = generateExp reg e1
-      (code2, reg2) = generateExp reg1 e2
-      multiplicationCode = "  %r" ++ show reg2 ++ " = mul i32 %r" ++ show (reg1 - 1) ++ ", %r" ++ show (reg2 - 1) ++ "\n"
-  in (code1 ++ code2 ++ multiplicationCode, reg2 + 1)
-
-generateExp reg (ExpDiv (ExpLit l1) (ExpLit l2)) =
-  ("  %r" ++ show reg ++ " = sdiv i32 " ++ show l1 ++ ", " ++ show l2 ++ "\n", reg + 1)
-generateExp reg (ExpDiv (ExpLit l) e) =
-  let (code, reg1) = generateExp reg e
-  in (code ++ "  %r" ++ show reg1 ++ " = sdiv i32 " ++ show l ++ ", %r" ++ show (reg1 - 1) ++ "\n", reg1 + 1)
-generateExp reg (ExpDiv e (ExpLit l)) =
-  let (code, reg1) = generateExp reg e
-  in (code ++ "  %r" ++ show reg1 ++ " = sdiv i32 %r" ++ show (reg1 - 1) ++ ", " ++ show l ++ "\n", reg1 + 1)
-generateExp reg (ExpDiv e1 e2) =
-  let (code1, reg1) = generateExp reg e1
-      (code2, reg2) = generateExp reg1 e2
-      divisionCode = "  %r" ++ show reg2 ++ " = sdiv i32 %r" ++ show (reg1 - 1) ++ ", %r" ++ show (reg2 - 1) ++ "\n"
-  in (code1 ++ code2 ++ divisionCode, reg2 + 1)
-
-generateExp reg (ExpLit n) =
-  ("  " ++ show n ++ "\n", reg)
-
-generateExp reg (ExpVar (Ident ident)) =
+generateExp code reg (ExpVar (Ident ident)) =
   let loadCode = "  %r" ++ show reg ++ " = load i32, i32* %" ++ ident ++ "\n"
-  in (loadCode, reg + 1)
+  in (loadCode : code, reg + 1)
+
+-- Funkcja do generowania operacji binarnych
+generateBinaryOp :: [String] -> Int -> AbsInstant.Exp -> AbsInstant.Exp -> String -> ([String], Int)
+generateBinaryOp code reg (ExpLit l1) (ExpLit l2) op =
+  let binOpCode = "  %r" ++ show reg ++ " = " ++ op ++ " i32 " ++ show (l1) ++ ", " ++ show (l2) ++ "\n"
+  in (binOpCode : code, reg + 1)
+generateBinaryOp code reg (ExpLit l) e2 op =
+  let (code1, reg1) = generateExp code reg e2
+      binOpCode = "  %r" ++ show reg1 ++ " = " ++ op ++ " i32 " ++ show (l) ++ ", %r" ++ show (reg1 - 1) ++ "\n"
+  in (binOpCode : code1, reg1 + 1)
+generateBinaryOp code reg e1 (ExpLit l) op =
+  let (code1, reg1) = generateExp code reg e1
+      binOpCode = "  %r" ++ show reg1 ++ " = " ++ op ++ " i32 %r" ++ show (reg1 - 1) ++ ", " ++ show (l) ++ "\n"
+  in (binOpCode : code1, reg1 + 1)
+generateBinaryOp code reg e1 e2 op =
+  let (code1, reg1) = generateExp code reg e1
+      (code2, reg2) = generateExp code1 reg1 e2
+      binOpCode = "  %r" ++ show reg2 ++ " = " ++ op ++ " i32 %r" ++ show (reg1 - 1) ++ ", %r" ++ show (reg2 - 1) ++ "\n"
+  in (binOpCode : code2, reg2 + 1)
